405c7ae873382f0f8125e521a293f785955368ba
tyvoiax <eadjeifrimpong@worcester.edu>
2018-06-12 01:37:52 -0400

    Connect nav links to various sections

--- index.js
+++ index.js
@@ -29,7 +29,51 @@ closeMenuIcon.addEventListener('click', (e) => {
 // Hide the menu
 window.onresize = () => {
     if (window.innerWidth > 768) {
+        menu.style.display = 'none';
+        document.documentElement.style.overflow = 'scroll';
+    }
+};
+
+// Smooth scrolling using jquery
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // Close menu before scrolling to the target
+    if (window.innerWidth < 768) {
         document.documentElement.style.overflow = 'scroll';
         menu.style.display = 'none';
     }
-};
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top - 100
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
+
